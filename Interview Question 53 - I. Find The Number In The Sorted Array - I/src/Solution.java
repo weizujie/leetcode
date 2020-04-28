5951bca7519639e6a7f3634ecbd8fbe2ae161d96
weizujie <weizujie98@gmail.com>
2020-04-28 12:31:02 +0800

    面试题53 - I 在排序数组中查找数字 I

---
+++ Interview Question 53 - I. Find The Number In The Sorted Array - I/src/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int count = 0;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] >= target) {
+                right = mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        while (left < nums.length && nums[left++] == target) {
+            count++;
+        }
+        return count;
+    }
+}
