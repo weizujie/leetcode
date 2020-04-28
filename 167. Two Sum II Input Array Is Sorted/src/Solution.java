d3e92d4d7d8b883c8c94259b08beed40eb27b9ef
weizujie <weizujie98@gmail.com>
2020-04-28 12:10:20 +0800

    167. 两数之和 II - 输入有序数组

---
+++ 167. Two Sum II Input Array Is Sorted/src/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+/**
+ * @Author: 韦祖杰
+ * @Date: 2020/4/6
+ * @Version: 1.0
+ * @Github: https://github.com/byojiaoxianz7
+ */
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+}
