2d77aa2afe9c2910d7f6f555f4c569f1d28ac502
weizujie <weizujie98@gmail.com>
2020-04-28 12:28:46 +0800

    374. 猜数字大小

---
+++ 374. Guess Number Higher Or Lower/src/Solution.java
@@ -0,0 +1,18 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int res = guess(mid);
+            if (res == 0) {
+                return mid;
+            } else if(res < 0) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return 0;
+    }
+}
