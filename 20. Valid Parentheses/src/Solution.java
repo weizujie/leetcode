de77ee75a20af75b20f49df5c6dd8eb4007a81cd
weizujie <weizujie98@gmail.com>
2020-05-12 15:43:18 +0800

    if (c == '(') {

---
+++ 20. Valid Parentheses/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+/**
+ * @Author: weizujie
+ * @Date: 2020/5/12
+ * @Version: 1.0
+ * @Github: https://github.com/weizujie
+ */
+public class Solution {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (Character c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || c != stack.pop()) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
